5e13542e0975fd7a02a7d78aa7cff308a45e2229
Kevin Djatschenko <kevin.djatschenko@mail.citytech.cuny.edu>
2020-09-28 15:33:01 -0400

    ok

---
+++ Prototype2020/Assets/UpandDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpandDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // object forward along z axis
+        transform.Translate(Vector3.forward * Time.deltaTime);
+
+        // object down along z axis
+        transform.Translate(Vector3.down * Time.deltaTime);
+    }
+}
