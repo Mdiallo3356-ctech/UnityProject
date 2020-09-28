a2b3b3d39634937cf03054e3d161342578707f24
Mamadou Diallo <mamadou.diallo13@mail.citytech.cuny.edu>
2020-09-28 14:39:08 -0400

    Made scene with cube movement.cs created jump implemented

---
+++ Prototype2020/Assets/movement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // public Transform myTransform;
+    private Rigidbody player; 
+    private float jumpVelocity = 5.5f;
+    private bool grounded;
+    private float groundedMovement = .0225f;
+    private float airMovement = .0125f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grounded = false; 
+        player = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(grounded == true)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                player.position += new Vector3(0, 0, groundedMovement);
+            }
+
+            if(Input.GetKey(KeyCode.A))
+            {
+                player.position -= new Vector3(groundedMovement, 0, 0);
+            }
+
+            if(Input.GetKey(KeyCode.S))
+            {
+                player.position -= new Vector3(0, 0, groundedMovement);
+            }
+
+            if(Input.GetKey(KeyCode.D))
+            {
+                player.position += new Vector3(groundedMovement, 0, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                player.velocity = Vector3.up * jumpVelocity;
+                grounded = false;
+            }
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                player.position += new Vector3(0, 0, airMovement);
+            }
+
+            if(Input.GetKey(KeyCode.A))
+            {
+                player.position -= new Vector3(airMovement, 0, 0);
+            }
+
+            if(Input.GetKey(KeyCode.S))
+            {
+                player.position -= new Vector3(0, 0, airMovement);
+            }
+
+            if(Input.GetKey(KeyCode.D))
+            {
+                player.position += new Vector3(airMovement, 0, 0);
+            }
+        }
+
+        
+
+        if(grounded == true){
+            
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        grounded = true;
+    }
+}
