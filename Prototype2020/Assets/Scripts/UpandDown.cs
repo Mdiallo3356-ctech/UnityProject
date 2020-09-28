985758ab1337a020cb0625a6f909d39edcee3d31
Kevin Djatschenko <kevin.djatschenko@mail.citytech.cuny.edu>
2020-09-28 16:17:20 -0400

    update

--- Prototype2020/Assets/Scripts/UpandDown.cs
+++ Prototype2020/Assets/Scripts/UpandDown.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class UpandDown : MonoBehaviour
 {
+    public int min;
+    public int max;
+    public bool increasing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        min = -5;
+        max = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // object forward along z axis
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        
+        
 
-        // object down along z axis
-        transform.Translate(Vector3.back * Time.deltaTime);
+        if (increasing == true)
+        {
+            // object forward along z axis
+            transform.Translate(Vector3.left * Time.deltaTime);
+            if (this.transform.position.x <= min)
+            {
+                increasing = false;
+            }
+        }
+
+        if (increasing == false)
+        {
+            // object down along z axis
+            transform.Translate(Vector3.right * Time.deltaTime);
+
+            if (this.transform.position.x >= max)
+            {
+                increasing = true;
+            }
+        }
     }
 }
